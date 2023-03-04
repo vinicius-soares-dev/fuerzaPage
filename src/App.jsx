d5cfdd4ea3e40b6959137419b9096b713ce2b73a
Vinicius Soares <106778671+vinicius-soares-dev@users.noreply.github.com>
2023-03-04 20:15:08 -0300

    addnavbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import NavBar from './components/Nav';
+
+import './styles/styleConfig.css';
+import './styles/styleNavbar.css';
+
+function App() {
+  return (
+    <div> 
+        <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const NavBar = () => {
+    return (
+        <nav className="Navigation">
+            <p className="nameText">Fuerza E-sports</p>
+            <ul>
+                <li>Sobre</li>
+                <li>Recrutamento</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
